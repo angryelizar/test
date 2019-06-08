22f6516ac476d29b40c216d502fb0fbfb2dcb157
Elizar <conovalov.elizar@gmail.com>
2019-06-09 02:28:24 +0600

    Коммит

---
+++ lesson1/js/script.js
@@ -0,0 +1,49 @@
+// var first = 1;
+// const pi = 3.14;
+
+// {
+//     let second = 2;
+// }
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// var und
+// console.log(und);
+
+// obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(obj["name"]);
+
+// var arr = ['plum', 'orange', 'apple'];
+// console.log(arr[2]);
+
+// // confirm("Дашь в жопу?");
+
+// var answer = +prompt("Сколько лет?", "18");
+
+// console.log( typeof(answer));
+// // console.log('arr' + " - object");
+// // console.log(4 + " - object");
+
+var incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(30%4);
+
+// console.log(incr == 10);
+
+var isChecked = true;
+var isClose = true;
+
+console.log(isChecked || !isClose);
+
